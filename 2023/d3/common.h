98578d0e6fbe240a573a754c5129132c0f70de6f
Bali Harko <balazs.harko@atg.se>
2024-05-10 11:01:33 +0200

    2023/d3/p2

--- 2023/d3/common.h
+++ 2023/d3/common.h
@@ -1,4 +1,10 @@
 #pragma once
 
+#define LINE_LENGTH 140 // 140 for input, 10 for example
+#define MAX_CHARS 65536 // 16 bit integer max number
+#define TOKEN_MAX 10
+
 int is_symbol(const char c);
+
+int is_digit(const char c);
 
--- 2023/d3/p1.c
+++ 2023/d3/p1.c
@@ -3,39 +3,34 @@
 #include <string.h>
 #include "common.h"
 
-#define LINE_LENGTH 140
-#define MAX_CHARS 65536 // 16 bit integer max number
-
 int main(void) {
     const char* input_str = input_as_cont_string("2023/d3/input.txt", MAX_CHARS);
-    int input_len = strlen(input_str);
+    const int input_len = strlen(input_str);
 
     int sum = 0;
     int i = 0;
-    char token[LINE_LENGTH] = { '\0' };
+    char token[TOKEN_MAX] = { '\0' };
     int tokenIdx = 0;
     while (i < input_len) {
-        if (input_str[i] >= '0' && input_str[i] <= '9')
+        if (is_digit(input_str[i]))
             token[tokenIdx++] = input_str[i];
-        else {
-            if (tokenIdx > 0) {
-                for (int j = i - tokenIdx - 1; j < i + 1; j++) {
-                    if (j < 0) continue;
-                    if (j % LINE_LENGTH == 0 || j % LINE_LENGTH == 0) continue;
+        else if (tokenIdx > 0){
+            for (int j = i - tokenIdx - 1; j < i + 1; j++) {
+                if (j < 0) continue;
+                if (j % LINE_LENGTH == 0 || j % LINE_LENGTH == 0) continue;
 
-                    if (is_symbol(input_str[j]))
-                        sum += atoi(token);
+                if (is_symbol(input_str[j]))
+                    sum += atoi(token);
 
-                    if (j > LINE_LENGTH && is_symbol(input_str[j - LINE_LENGTH]))
-                        sum += atoi(token);
+                if (j > LINE_LENGTH && is_symbol(input_str[j - LINE_LENGTH]))
+                    sum += atoi(token);
 
-                    if (j + LINE_LENGTH < input_len && is_symbol(input_str[j + LINE_LENGTH]))
-                        sum += atoi(token);
-                }
-                
-                memset(token, '\0', sizeof(token));
-                tokenIdx = 0;
+                if (j + LINE_LENGTH < input_len && is_symbol(input_str[j + LINE_LENGTH]))
+                    sum += atoi(token);
             }
+
+            memset(token, '\0', sizeof(token));
+            tokenIdx = 0;
         }
 
         i++;
